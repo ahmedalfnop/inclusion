745ac6253c881a93ec25946ede89ecc1554e5308
Maxim Orlov <mail@orlovmax.com>
2015-06-21 02:17:35 +0300

    remove minified src in dev version

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -3,23 +3,12 @@ module.exports = {
 	options: {
 		mangle: false
 	},
-	vendor: {
-		files: [{
-				expand: true,
-				cwd: '<%= build.js %>/vendor',
-				src: ['**/*.js', '!*.min.js'],
-				dest: '<%= build.js %>/vendor',
-				ext: '.min.js'
+	build: {
+		files:[{
+			expand: true,
+			cwd: '<%= build.js %>/',
+			src: ['**/*.js', '!**/*.min.js'],
+			dest: '<%= build.js %>/'
 		}]
-	},
-	main: {
-			files: {
-					'<%= build.js %>/inclusion.min.js': '<%= build.js %>/inclusion.js'
-			}
-	},
-	ie: {
-		files: {
-				'<%= build.js %>/ie/ie.min.js': '<%= build.js %>/ie/ie.js'
-		}
 	}
 }
